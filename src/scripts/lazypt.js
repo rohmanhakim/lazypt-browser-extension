d468fedd42073b9e7adbe4b1e011ffe36cd4a056
Muhammad Arif Rohman Hakim <rohmanhakim@live.com>
2021-11-03 21:48:29 +0700

    add two unfunctional buttons for each story element

--- src/scripts/lazypt.js
+++ src/scripts/lazypt.js
@@ -1,8 +1,75 @@
 export default class LazyPt {
 
-  constructor() {  }
-  
+  constructor() {
+    this.defaultPanel = 'my-work';
+  }
+
   onLoad() {
-    document.querySelector('section.main').style.backgroundColor = '#ff00ff';
+    this.appendButtonsToStories();
+  }
+
+  appendButtonsToStories() {
+    // set up the mutation observer
+    // `mutations` is an array of mutations that occurred
+    // `me` is the MutationObserver instance
+    let observer = new MutationObserver((mutations, me) => {
+      let panel = document.querySelector(".panel[data-type='my_work']");
+      if (panel) {
+        this.getAllStoryElements(panel);
+        me.disconnect();
+        return;
+      }
+    });
+
+    // start observing
+    observer.observe(document, {
+      childList: true,
+      subtree: true
+    });
+  }
+
+  getAllStoryElements(panel) {
+    let stories = panel.querySelectorAll(".story[data-aid='StoryPreviewItem']");
+    stories.forEach(story => this.appendButtons(story))
+  }
+
+
+  appendButtons(storyElement) {
+    let buttonContainer = this.createButtonscontainer();
+    let storyHeader = storyElement.firstChild;
+    let storyHeaderName = storyHeader.querySelector('.name')
+    storyHeader.style.minHeight = '124px';
+    storyHeader.appendChild(buttonContainer);
+    storyHeaderName.style.justifyContent = 'inherit';
+    console.log('story : ' + storyElement);
+  }
+
+  createButtonscontainer() {
+    let buttonContainer = document.createElement("div");
+    buttonContainer.classList.add('lazypt-button-container');
+    buttonContainer.appendChild(this.createButtoncontainerHeader());
+    buttonContainer.appendChild(this.createBranchNameButton());
+    buttonContainer.appendChild(this.createCommitMessageButton());
+    return buttonContainer;
+  }
+
+  createBranchNameButton() {
+    let button = document.createElement("button");
+    button.textContent = "Branch Name";
+    button.classList.add('lazypt-button');
+    return button;
+  }
+
+  createCommitMessageButton() {
+    let button = document.createElement("button");
+    button.textContent = "Commit Message";
+    button.classList.add('lazypt-button');
+    return button;
+  }
+
+  createButtoncontainerHeader() {
+    let header = document.createElement("p");
+    header.textContent = "Generate:"
+    return header;
   }
 }
